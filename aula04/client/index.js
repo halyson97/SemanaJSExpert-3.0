d586651ce37f2679ddcf198a8c7e9ccf410cf63f
Halyson <halyson97@gmail.com>
2021-03-27 15:08:23 -0300

    Aula 04 - Notificando logs de usuários e enviando mensagens por salas

---
+++ aula04/client/index.js
@@ -0,0 +1,37 @@
+
+import Events from 'events';
+import CliConfig from './src/cliConfig.js';
+import TerminalController from "./src/terminalController.js";
+import SocketClient from './src/socket.js'
+import EventManager from './src/eventManager.js';
+
+const [nodePath, filePath, ...comands] = process.argv
+const config = CliConfig.parseArguments(comands)
+
+console.log(`config`, config)
+
+const componentEmitter = new Events()
+
+const socketClient = new SocketClient(config)
+
+await socketClient.initialize()
+
+const eventManager = new EventManager({ componentEmitter, socketClient })
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+    roomId: config.room,
+    userName: config.username
+}
+
+eventManager.joinRoomAndWaitForMessages(data)
+
+
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
+
+
+
+
+
